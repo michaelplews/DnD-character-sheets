5d902e975ba523459d3c23de44e9f29f4b5d4e2e
Mark Wolfman <canismarko@gmail.com>
2018-03-27 13:26:35 -0500

    Added Inara character sheet.

---
+++ inara.py
@@ -0,0 +1,46 @@
+name = 'Inara Serradon'
+character_class = 'wizard'
+player_name = 'Mark'
+background = "Acolyte"
+race = "High-Elf"
+level = 3
+alignment = "Chaotic good"
+xp = 2190
+hp_max = 16
+speed = 30
+
+# Ability Scores
+strength = 10
+dexterity = 15
+constitution = 14
+intelligence = 16
+wisdom = 12
+charisma = 8
+skill_proficiencies = [
+    'arcana',
+    'insight',
+    'investigation',
+    'perception',
+    'religion',
+]
+
+# Inventory
+cp = 316
+sp = 283
+ep = 28
+gp = 125
+pp = 0
+
+# Backstory
+personality_traits = """I use polysyllabic words that convey the impression of
+erudition. Also, I’ve spent so long in the temple that I have little
+experience dealing with people on a casual basis."""
+
+ideals = """Knowledge. The path to power and self- improvement is through
+knowledge."""
+
+bonds = """The tome I carry with me is the record of my life’s work so far,
+and no vault is secure enough to keep it safe."""
+
+flaws = """I’ll do just about anything to uncover historical secrets that
+would add to my research."""
