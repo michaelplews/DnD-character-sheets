d8543df6f3f2742245b916b987ca2fc650f168a5
Mark Wolfman <canismarko@gmail.com>
2018-04-06 17:22:53 -0500

    Added 'Io' warlock character from one-shot.

---
+++ io.py
@@ -0,0 +1,69 @@
+name = 'Io Desolata'
+character_class = 'Warlock'
+player_name = 'Mark'
+background = "Criminal"
+race = "Tiefling"
+level = 1
+alignment = "Neutral evil"
+xp = 0
+hp_max = 9
+
+# Ability Scores
+strength = 10
+dexterity = 10
+constitution = 13
+intelligence = 12
+wisdom = 11
+charisma = 15
+skill_proficiencies = [
+    # All lower case, underscores for spaces
+    'deception', 'sleight_of_hand',
+]
+
+# Proficiencies and languages
+languages = "Common, infernal."
+
+# Inventory
+cp = 0
+sp = 0
+ep = 0
+gp = 15
+pp = 0
+weapons = ('light crossbow', 'quarterstaff', 'dagger')
+equipment = ("""Dark orb (Arcane focus), Book of lore ink pen, little bag of sand,
+Bolts (20), set of weighted dice, belt pouch Backpack, bottle of ink,
+10 sheets of parchment, small knife, a set of dark clothes, crowbar""")
+
+# List of known spells
+spells = ('thaumaturgy', 'eldritch blast', 'poison spray',
+    'armor of agathys', 'burning hands')
+# Which spells have been prepared (not including cantrips)
+spells_prepared = ()
+
+# Backstory
+personality_traits = """"""
+
+ideals = """"""
+
+bonds = """"""
+
+flaws = """"""
+
+features_and_traits = (
+    """**Darkvision** Thanks to your infernal heritage, you have superior
+    vision in dark and dim conditions. You can see in dim light within
+    60 feet of you as if it were bright 1ight, and in darkness as if
+    it were dim light. You can’t dis- cern color in darkness, only
+    shades of gray.
+    
+    **Hellish Resistance** You have resistance to fire damage.
+    
+    **Infernal Legacey** You know the thaumatuqJy cantrip.  Once you
+    reach 3rd level, you can cast the hellish rebuke spell once per
+    day as a 2nd-levei spell. Once you reach 5th level, you can also
+    cast the darkness spell once per day.  Charisma is your
+    spellcasting ability for these spells.
+   
+    **Dark One’s Blessing (The Fiend)** When you reduce a hostile
+    creature to O hit-points, you gain temporary hit points equal to
+    your Charisma modifier + your warlock level (minimum of 1).""")
